eecd3c49e2115521661def29845c68f9fa088d1c
DanFerrari <danieldasilvaferrari@gmail.com>
2025-03-27 16:34:05 -0300

    calibrado...

--- campimetria/procedures/Ponto.py
+++ campimetria/procedures/Ponto.py
@@ -193,6 +193,8 @@ class Ponto:
                     pygame.time.get_ticks() - trial_start_time
                 ) / 1000  
                 self.response_received = True 
+                self.apagarPonto()
+                pygame.time.delay(500)
                 return menu_pressionado
 
             if not self.response_received:
