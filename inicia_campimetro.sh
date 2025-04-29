f7362b33b2c33ce108d7cababc09a1ec820ebe81
DanFerrari <danieldasilvaferrari@gmail.com>
2025-04-29 10:05:43 -0300

    Update inicia_campimetro.sh to change directory and execute main.py with necessary commands

--- inicia_campimetro.sh
+++ inicia_campimetro.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 #sudo su
-cd /home/eyetec/TA-3rdGen/lib/campimetria/pages
-sudo python3 TelaInicial.py & sudo xmodmap -e "pointer = 0 0 0 0 0 0 0 0 0 0"
+cd /home/eyetec/TA-3rdGen/lib/campimetria/
+sudo python3 main.py & sudo irxevent /home/eyetec/.lircrc & sudo xmodmap -e "pointer = 0 0 0 0 0 0 0 0 0 0"
 
-#& sudo irxevent /home/eyetec/.lircrc 
 
 
